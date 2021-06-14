ff3cb5733c2c292ed1c529b3284e1cae07493c13
Niraj-kumar99 <kumarnkj35@gmail.com>
2021-06-14 10:13:42 +0530

    Runs upto 21 times

--- flipCoinSimulator2.sh
+++ flipCoinSimulator2.sh
@@ -1,18 +1,16 @@
 #!/bin/bash 
-count=1
 heads=0
 tails=0
-while [ $count -le 10 ]
+while (( "$heads" < 21 || "$tails" < 21 ))
 do
 FLIP=$(($(($RANDOM%10))%2))
 if [ $FLIP -eq 1 ];then
 	echo "heads"
 	((heads++))
-((count++))	
+
 else
     	echo "tails"
 	((tails++))
-((count++))
 
 fi
 echo $heads
@@ -22,6 +20,10 @@ done
 if [ $heads -gt $tails ]
 then
 echo "WINNER IS : HEAD"
+
+elif [ $heads -eq $tail ]
+then
+echo "IT'S TIE"
 
 else
 echo "WINNER IS : TAIL"
